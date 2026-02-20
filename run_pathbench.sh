602fd104fb409d3c0353131b7a3b5d8c2de2ce3e
jorgeBIGS <jorgarcia@us.es>
2026-02-20 19:58:30 +0100

    Cambios realizados para ejecución en cluster

--- run_pathbench.sh
+++ run_pathbench.sh
@@ -1,19 +1,9 @@
-#If virtual environment does not exist, construct one using pip
-if [ ! -d "pathbench_env" ]; then
-    python3 -m venv pathbench_env
-    source pathbench_env/bin/activate
-    pip install --upgrade pip
-    pip install -r requirements.txt
-else
-    source pathbench_env/bin/activate
-fi
-
 #Set slideflow backends
 export SF_SLIDE_BACKEND=cucim
 export SF_BACKEND=torch
 
 #Set the config file
-CONFIG_FILE=conf.yaml
+CONFIG_FILE=$1
 
 #Run the program
 python3 main.py --config $CONFIG_FILE
